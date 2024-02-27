02bd5220b14c7228834d4ea8acb277653d0a07b5
Alan Silva <silzen.ent@gmail.com>
2024-02-26 20:44:43 -0700

    Completed Lab 1: Flask SQL ALchemy

--- server/app.py
+++ server/app.py
@@ -21,6 +21,42 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+
+    if earthquake: 
+        body = earthquake.to_dict()
+        status = 200
+    else: 
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+    
+    return make_response(body, status)
+
+
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquakes_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+
+    body = {
+        'count': len(earthquakes),
+        'quakes': [earthquake.to_dict() for earthquake in earthquakes]
+    }
+
+    if earthquakes: 
+        status = 200
+    else: 
+        # body['message'] = f'No earthquakes with magnitude greater than or equal to {magnitude} found.'
+        status = 200
+    
+    return make_response(body, status)
+
+    # if not earthquakes: 
+    #     body['message'] = f'No earthquakes with magnitude greater than or equal to {magnitude} found.'
+    
+    # return make_response(body, 200)  # always return 200 status code
 
 
 if __name__ == '__main__':
